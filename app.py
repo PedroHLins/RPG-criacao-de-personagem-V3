18d37c8e1c1144a11289b1faba6e5f04e50e6b80
PedroHLins <ped971888@gmail.com>
2025-09-15 20:52:40 -0300

    feat - Criando personagem e conectando informações ao front

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from services.PersonagemService import PersonagemService
 
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_principal():
     return render_template("index.html")
+
+@app.route("/Criar-Persongaem", methods=["POST"])
+def criar_novo_personagem():
+    service = PersonagemService()
+    personagem = service.criar_persongaem(request.form)
+    return render_template("ficha.html", personagem=personagem)
 
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ services/PersonagemService.py
@@ -0,0 +1,40 @@
+from unittest import case
+
+from models.Personagem import Personagem
+from models.classes.Gurreiro import Guerreiro
+from models.classes.Ladrao import Ladrao
+from models.classes.Mago import Mago
+from models.racas.Anao import Anao
+from models.racas.Elfo import Elfo
+from models.racas.Halfling import Halfling
+from models.racas.Humano import Humano
+
+class PersonagemService:
+    def escolher_raca(self, raca):
+        match raca:
+            case "Humano":
+                return Humano()
+            case "Anao":
+                return Anao()
+            case "Elfo":
+                return Elfo()
+            case "Halfling":
+                return Halfling()
+        return None
+
+
+    def escolher_classe(self, classe):
+        match classe:
+            case "Guerreiro":
+                return Guerreiro()
+            case "Mago":
+                return Mago()
+            case "Ladrao":
+                return Ladrao()
+        return None
+
+    def criar_persongaem(self, dados_formulario):
+        raca = self.escolher_raca(dados_formulario.get("raca"))
+        classe = self.escolher_classe(dados_formulario.get("classe"))
+        novo_personagem = Personagem(dados_formulario.get("char-name"), raca, classe)
+        return novo_personagem
