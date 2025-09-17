af1dc05f62a765ba38757879e3e63482b1ca6358
PedroHLins <ped971888@gmail.com>
2025-09-17 18:07:44 -0300

    feat - Interligação das páginas

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 
 from services.PersonagemService import PersonagemService
+from util.AventuraUtil import Aventura
+from util.DistribuicaoUtil import Distribuicao
 
 app = Flask(__name__)
 
@@ -11,8 +13,25 @@ def pagina_principal():
 @app.route("/Criar-Persongaem", methods=["POST"])
 def criar_novo_personagem():
     service = PersonagemService()
-    personagem = service.criar_persongaem(request.form)
-    return render_template("ficha.html", personagem=personagem)
+    personagem = service.criar_persongaem_inicial(request.form)
+    tipo_aventura = request.form["aventura"]
+
+    if tipo_aventura == "Classica":
+        valores = Aventura.classico_aventureiro()
+        Distribuicao.classico(personagem, valores)
+        return render_template("ficha.html", personagem=personagem)
+    elif tipo_aventura == "Aventureira":
+        valores = Aventura.classico_aventureiro()
+    else:
+        valores = Aventura.heroico()
+
+    return render_template("distribuicao.html", personagem=personagem, valores=valores)
+
+@app.route("/Ficha", methods=["POST"])
+def exibir_ficha():
+    personagem_service = PersonagemService()
+    personagem_final = personagem_service.criar_persongaem_final(request.form)
+    return render_template("ficha.html", personagem=personagem_final)
 
 if __name__ == "__main__":
     app.run(debug = True)
--- services/PersonagemService.py
+++ services/PersonagemService.py
@@ -1,5 +1,3 @@
-from unittest import case
-
 from models.Personagem import Personagem
 from models.classes.Gurreiro import Guerreiro
 from models.classes.Ladrao import Ladrao
@@ -8,6 +6,8 @@ from models.racas.Anao import Anao
 from models.racas.Elfo import Elfo
 from models.racas.Halfling import Halfling
 from models.racas.Humano import Humano
+from util.DistribuicaoUtil import Distribuicao
+
 
 class PersonagemService:
     def escolher_raca(self, raca):
@@ -33,8 +33,13 @@ class PersonagemService:
                 return Ladrao()
         return None
 
-    def criar_persongaem(self, dados_formulario):
+    def criar_persongaem_inicial(self, dados_formulario):
         raca = self.escolher_raca(dados_formulario.get("raca"))
         classe = self.escolher_classe(dados_formulario.get("classe"))
         novo_personagem = Personagem(dados_formulario.get("char-name"), raca, classe)
         return novo_personagem
+
+    def criar_persongaem_final(self, dados_formulario):
+        personagem = self.criar_persongaem_inicial(dados_formulario)
+        Distribuicao.aventureiro_heroico(personagem, dados_formulario)
+        return personagem
